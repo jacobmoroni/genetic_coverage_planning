9b6df416a338c31016f9cfb20f311862e7f1080e
Craig Bidstrup <drinkdhmo@gmail.com>
2019-03-20 14:08:38 -0600

    added dots and working on moving dots away from walls.

--- pather_driver.py
+++ pather_driver.py
@@ -13,8 +13,9 @@ from pathmaker import PathMaker
 file_name = "map_scaled.png"
 scale = 0.15
 narrowest_hall = 1.75
+safety_buffer = 0.7
 map_scaled = cv2.imread(file_name,cv2.IMREAD_GRAYSCALE)
 
-pather = PathMaker(map_scaled, scale, narrowest_hall)
+pather = PathMaker(map_scaled, scale, narrowest_hall, safety_buffer)
 
 pather.smartly_place_dots()
--- pathmaker.py
+++ pathmaker.py
@@ -6,18 +6,51 @@ import cv2
 import matplotlib.pyplot as plt
 
 class PathMaker(object):
-    def __init__(self, mappy, scale, hall_width):
-        self.mappy = mappy
-        self.scale = scale
-        self.hall_width = hall_width
+    def __init__(self, mappy, scale, hall_width, safety_buffer):
+        self._mappy = mappy/255
+        self._scale = scale
+        self._hall_width = hall_width
+        self._safety_buffer = safety_buffer
+        size = self._mappy.shape
+        # self._grid = np.mgrid[0:size[0]*scale:scale, 0:size[1]*scale:scale]
     #
     def smartly_place_dots(self):
-        self.dots = np.zeros_like(self.mappy)
-        stride = int(self.hall_width//self.scale)
-        print(self.dots.shape)
-        for i in 
-        self.dots[0:stride:-1, 0:stride:-1] = 255
-        cv2.imshow('dots',self.dots)
+        self.pac_dots = np.zeros_like(self._mappy)
+        stride = int((self._hall_width)//self._scale)
+        print(self.pac_dots.shape)
+        size = self._mappy.shape
+        X,Y = np.mgrid[0:size[0]:stride,0:size[1]:stride]
+        XY = np.vstack((X.flatten(), Y.flatten())).T
+        XY = XY[self._mappy[XY[:,0], XY[:,1]] == 0]
+        # now we need to move dots to be at least safety_buffer away from obstacles
+        obstacles = (np.array(np.nonzero(self._mappy)) * self._scale)
+        # distances = np.zeros((XY.shape[0], obstacles.shape[0]))
+        # angles = np.zeros_like(distances)
+        displacements = np.array([obstacles[None, 0] - XY[:,0, None], obstacles[None, 1] - XY[:,1, None]])
+        print(displacements.shape)
+        distances = np.linalg.norm(displacements, axis=0)
+        angles = np.arctan2(displacements[1], displacements[0])
+        print(distances.shape)
+        print(angles.shape)
+        # for i, obs in enumerate(XY):
+        #
+        #     for j, point in enumerate(obstacles):
+        #         distances[i,j] = np.linalg.norm(obs - point);
+        #         angles[i,j] = np.arctan2(obs[1] - point[1], obs[0] - point[0])
+
+        closest = np.argmin(distances, axis=1)
+        print(closest.shape)
+        idx = distances.T[closest]<self._safety_buffer
+        print(idx.shape)
+        XY[idx,0] + (self._safety_buffer-distances[idx])*np.cos(angles[idx])
+        XY[idx,1] - (self._safety_buffer-distances[idx])*np.sin(angles[idx])
+
+        num_dilations = int(self._safety_buffer/self._scale)
+        kernel = np.ones((3,3),np.uint8)
+        map_dilated = cv2.dilate(self._mappy,kernel,iterations = num_dilations)
+        self.pac_dots[XY[:,0], XY[:,1]] = 1
+        # self.pac_dots = self.pac_dots*(1-self._mappy)
+        cv2.imshow('pac_dots',self.pac_dots + 0.25*self._mappy + 0.25*map_dilated)
         cv2.waitKey()
     #
 #
