0a4bc2c5d859836ab062f2f5779c94182bb853fa
Brady Anderson <brady.bus@gmail.com>
2019-03-28 08:50:12 -0600

    started maximin

--- geneticalgorithm.py
+++ geneticalgorithm.py
@@ -15,7 +15,7 @@ from pathmaker import PathMaker
 class GeneticalGorithm( ):
     def __init__( self, mappy, scale, narrowest_hall, max_dna_len, pather ):
         # setup params
-        self._G_sz = 10
+        self._G_sz = 10 # has to be an EVEN number !!!!!
         self._G_num = 10
         self._T_sz = 4
         self._gamma = 0.5
@@ -26,25 +26,36 @@ class GeneticalGorithm( ):
 
         # list for holding all chromosomes in parent generation
         self._gen_parent = []
+        self._gen_parent_fit = []
         # list for holding all chromosomes in children generation
         self._gen_child = []
+        self._gen_child_fit = []
+
+        self._gen_pc = []
+        self._gen_pc_fit = []
 
         # create list of first generation
         self.firstGeneration(mappy, scale, narrowest_hall, max_dna_len, pather)
 
         # start species history
         for ii in range(self._G_num):
-            pass
+            for jj in range(self._G_sz * 0.5):
+
         #
     #
     def firstGeneration(self, mappy, scale, narrowest_hall, max_dna_len, pather):
         # create random set of chromosomes
         path_length = 200
         start_idx = 207
+        # pool this for loop with multiprocess
         for ii in len(self._G_sz):
-            rand_path = pather.makeMeAPath(path,start_idx)
+            rand_path = pather.makeMeAPath(path_length,start_idx)
             self._gen_parent.append( Organism(rand_path, mappy, scale, narrowest_hall, max_dna_len, pather) )
+            self._gen_parent_fit.append(self._gen_parent[ii]._fitness)
         #
+        self.maximin()
+        # TODO sort according to fitness function
+
     #
     def selection(self):
         # tourny or [ roulette ]
@@ -61,7 +72,35 @@ class GeneticalGorithm( ):
     #
     # scoring vs ranking vs [ maximin ]
     def maximin(self):
-        pass
+        # TODO: obj1 is coverage --> MINIMIZING the NEGATIVE
+        # obj2 is flight-time --> MINIMIZING
+        # obj1 = -self._fitness[0]
+        # obj1 = self._fitness[1]
+
+        comp_min = []
+        comp_max = []
+
+        gen_pc_fit = []
+        gen_pc_fit.append(self._gen_parent_fit)
+        gen_pc_fit.append(self._gen_child_fit)
+
+        for ii, gen_fit_1 in enumerate(gen_pc_fit):
+            for jj, gen_fit_2 in enumerate(gen_pc_fit):
+                if ii == jj:
+                    continue
+                else:
+                    comp_min.append( np.min([gen_fit_1[0]-gen_fit_2[0], gen_fit_1[1]-gen_fit_2[1]]) )
+                #
+            #
+            comp_max.append( np.max(comp_min) )
+        #
+        sort_idx = np.argsort(comp_max)
+        gen_pc = np.append
+        self._gen_pc.append(self._gen_parent)
+        self._gen_pc.append(self._gen_child)
+
+        self._gen_pc_fit.append(self._gen_parent)
+        self._gen_pc_fit.append(self._gen_child)
     #
 #
 
@@ -114,7 +153,10 @@ class Organism( ):
         # apply mutation
         self.mutation()
 
-        self.obj_val = self.calcObj()
+        self._obj_val = self.calcObj()
+        # self._obj_val_2 = self.calcObj2()
+
+        self._fitness = self.maximin()
     #
     def calcObj(self):
         coverage = self._mappy.getCoverage(self._dna)
@@ -184,6 +226,8 @@ class Organism( ):
         elif self._len_dna < self._max_dna_len:
             self._dna = np.append( self._dna, np.ones(self._max_dna_len - self._len_dna) * -1 ).astype(int)
         #
+    #
+
 #
 
 # class Generation( ):
